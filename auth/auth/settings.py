391b07976a11982c966f2aa1587c189d80febc05
bhavikx <64426653+bhavikx@users.noreply.github.com>
2021-06-18 15:12:36 +0530

    Update settings.py

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'adlpo+j8ve&#5cppn&#*x+t%q85%y3y^vdy9_$vsi&!qm%w!-i'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
